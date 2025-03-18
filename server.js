c18f3f49ce6f4d0e9c1a72f09f21e9d57ad16665
laljiParmar3572 <laljiparmar11221@gmail.com>
2025-03-18 20:31:31 +0530

    first commit1

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = require('../task-manager/routes/index.js');
+const router = require('./routes/index.js');
 const connectDB = require('./config/db.js');
 const app = express();
 app.use(router);
