57192fa6a78d1dc42a592c79cd86af8a3eb099b6
laljiParmar3572 <laljiparmar11221@gmail.com>
2025-03-25 20:08:03 +0530

    add atlas

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 
 const connectDB=async()=>{
-    const uri = "mongodb://ral_developer:Raxu@@0609@test-shard-00-00.sjc45.mongodb.net:27017,test-shard-00-01.sjc45.mongodb.net:27017,test-shard-00-02.sjc45.mongodb.net:27017/?replicaSet=atlas-14i29i-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=test";
+    const uri = "mongodb+srv://ral_developer:Raxu@@0609@test.sjc45.mongodb.net/?appName=test";
     try{
     await mongoose.connect("mongodb://localhost:27017/task");
     console.log('connected');
