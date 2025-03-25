94a928299ced9e71ef83e93d7e3c69523c8c8b22
laljiParmar3572 <laljiparmar11221@gmail.com>
2025-03-25 19:44:06 +0530

    add atlas

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose');
 
 const connectDB=async()=>{
+    const uri = "mongodb+srv://ral_developer:Raxu@@0609@test.sjc45.mongodb.net/?appName=test";
     try{
     await mongoose.connect("mongodb://localhost:27017/task");
     console.log('connected');
