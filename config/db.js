81d4e290f9d3f05f7dae0c26110bfd94bedd75c0
laljiParmar3572 <laljiparmar11221@gmail.com>
2025-03-31 20:05:32 +0530

    initial

--- config/db.js
+++ config/db.js
@@ -1,9 +1,11 @@
-const mongoose=require('mongoose');
+require('dotenv').config();
+const mongoose = require('mongoose');
 
 const connectDB=async()=>{
     try{
-      const url='mongodb+srv://ral_developer:Raxu@@0609@test.sjc45.mongodb.net/?retryWrites=true&w=majority&appName=test';
-    await mongoose.connect('mongodb+srv://ral_developer:Lalji123@test.sjc45.mongodb.net/?retryWrites=true&w=majority&appName=test');
+      const uri = process.env.DB_URI.replace("${DB_PASSWORD}", process.env.DB_PASSWORD);
+
+    await mongoose.connect(uri);
     console.log('connected');
     }catch(error){
         console.log('error --->',error);
